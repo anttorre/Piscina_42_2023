cb15b507cf9ffc0ab536096579ce009a1e46593a
anttorre <125140017+anttorre@users.noreply.github.com>
2023-02-20 15:44:05 +0100

    Add files via upload

--- c_04/ex03/ft_atoi.c
+++ c_04/ex03/ft_atoi.c
@@ -6,23 +6,50 @@
 /*   By: anttorre <atormora@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 11:14:02 by anttorre          #+#    #+#             */
-/*   Updated: 2023/02/20 11:54:25 by anttorre         ###   ########.fr       */
+/*   Updated: 2023/02/20 15:41:14 by anttorre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int ft_atoi(char *str)
+int	ft_atoi(char *str)
 {
-    
+	int	num;
+	int	i;
+	int	sign;
+
+	num = 0;
+	i = 0;
+	sign = 1;
+	while (str[i] <= ' ' || str[i] == '\n' || str[i] == '\t')
+	{
+		i++;
+	}
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+		{
+			sign = -1;
+		}
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		printf("%d\n", num);
+		num = num * 10 + (str[i] - 48);
+		i++;
+	}
+	return (sign * num);
 }
 
-int	main(int argc, char const *argv[])
+int	main(int argc, char *argv[])
 {
+	int	a;
+
 	if (argc > 1)
 	{
-        int a = atoi(argv[1]);
+		a = ft_atoi(argv[1]);
 		printf("%d\n", a);
 	}
 	return (0);
